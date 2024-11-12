20e4104818736a1da23f47a55bc72232f980175b
Harald Klimach <harald.klimach@dlr.de>
2024-11-12 16:06:48 +0100

    Check schemes for multi-level compatibility
    As we currently only support acoustic scaling, not all schemes can be
    computed on multi-level meshes.
    With this check we ensure that only compatible schemes are computed on
    multi-level meshes.

--- source/scheme/mus_scheme_header_module.f90
+++ source/scheme/mus_scheme_header_module.f90
@@ -233,30 +233,6 @@ contains
       &                      trim(me%relaxHeader%variant)
     call tem_horizontalSpacer(fUnit = logUnit(1))
 
-    ! Both multispeciees and poisson equation must have diffusive scaling
-    ! since diffusive scaling is used to recover macroscopic equations
-    ! from asymptotic analysis
-    select case(trim(me%kind))
-    case ('fluid', 'fluid_incompressible', 'isotherm_acEq' )
-      if (trim(scaling) /= 'acoustic') then
-         call tem_abort('ERROR: Choose scaling = "acoustic" for ' &
-           &            // trim(me%kind))
-      end if
-    case ( 'multispecies_gas', 'multispecies_liquid', 'nernst_planck', &
-      &    'passive_scalar, ''poisson', 'poisson_boltzmann_linear',    &
-      &    'poisson_boltzmann_nonlinear'                               )
-      ! todo: with the removal of the scaling option from the configuration,
-      !       we need to do something here, otherwise all those schemes get
-      !       disabled.
-      !       It would be best to check for multilevel meshes, and complain
-      !       about them, in the case of these schemes.
-      !       Doesn't have to be here, though.
-      if(trim(scaling) /= 'diffusive') then
-         call tem_abort('ERROR: Choose scaling = "diffusive" for ' &
-           &            // trim(me%kind))
-      end if
-    end select
-
   end subroutine mus_load_scheme_header
   ! ************************************************************************** !
 
