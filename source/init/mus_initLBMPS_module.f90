1642bda4ffa8b18f44ed310492768f74cff5b48a
MikeW097 <52669061+MikeW097@users.noreply.github.com>
2025-11-18 23:14:43 +0100

    Update source/init/mus_initLBMPS_module.f90
    Co-authored-by: haraldkl <harald.klimach@dlr.de>

--- source/init/mus_initLBMPS_module.f90
+++ source/init/mus_initLBMPS_module.f90
@@ -82,8 +82,8 @@ contains
         case( 'd3q19' )
           compute => mus_advRel_kPS_rBGK_vEmodel_lD3Q19
         case default
-          write(logUnit(1),*) 'The selected layout is not supported: '//       &
-            &                  trim(layout)
+          write(logUnit(1),*) 'The selected layout is not supported: ' &
+            &                  // trim(layout)
           call tem_abort()
         end select
       case( 'EmodelCorr' )
