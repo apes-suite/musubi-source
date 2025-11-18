a2cc27e2a921a38a5b25e71a96af7f7b36009365
MikeW097 <52669061+MikeW097@users.noreply.github.com>
2025-11-18 23:16:17 +0100

    Update source/init/mus_initLBMPS_module.f90
    Co-authored-by: haraldkl <harald.klimach@dlr.de>

--- source/init/mus_initLBMPS_module.f90
+++ source/init/mus_initLBMPS_module.f90
@@ -129,8 +129,8 @@ contains
         case( 'd3q19' )
           compute => mus_advRel_kPS_rTRT_vLmodel_lD3Q19
         case default
-          write(logUnit(1),*) 'The selected layout is not supported: '//       &
-            &                  trim(layout)
+          write(logUnit(1),*) 'The selected layout is not supported: ' &
+            &                  // trim(layout)
           call tem_abort()
         end select
       case default
