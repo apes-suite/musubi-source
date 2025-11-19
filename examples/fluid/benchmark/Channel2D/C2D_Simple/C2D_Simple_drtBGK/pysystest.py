b6b3a150e08442e9dbe5ae328f33e4f95ab40ecc
Harald Klimach <harald.klimach@dlr.de>
2025-11-19 18:24:35 +0100

    Update reference files for examples

--- examples/fluid/benchmark/Channel2D/C2D_Simple/C2D_Simple_drtBGK/pysystest.py
+++ examples/fluid/benchmark/Channel2D/C2D_Simple/C2D_Simple_drtBGK/pysystest.py
@@ -23,6 +23,8 @@ class PySysTest(ApesHelper, pysys.basetest.BaseTest):
 
     def validate(self):
         self.apes.checkMusLog()
+        # NOTE: this reference file was generated with a debug executable,
+        #       which resulted in an actual filename with 13.334 instead!
         trackfile = 'channel_pressAlongLength_p00000_t13.335E+00.res'
         self.assertPathExists('tracking/'+trackfile,
                               abortOnError = True)
